8439b9200f963f633db8ed908c22e0ba011e2426
Zukhriddin <zuxr.akhmedov@gmail.com>
2021-09-17 12:00:26 +0500

    fetched the backend

--- src/components/blog/blog-list/index.jsx
+++ src/components/blog/blog-list/index.jsx
@@ -3,6 +3,18 @@ import { Row, Col } from "react-bootstrap";
 import BlogItem from "../blog-item";
 import posts from "../../../data/posts.json";
 export default class BlogList extends Component {
+
+  state = {
+    post: []
+  }
+
+  async componentDidMount() {
+    const res = await fetch("http://localhost:3002/posts")
+    const posts = await res.json()
+    //console.log(posts)
+    this.setState({ posts })
+  }
+
   render() {
     return (
       <Row>
